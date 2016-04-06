cf9698d8d75c8b7e18be947a32ce9eaacc6b31e1
Tyler Fisher <tyler@tylerjfisher.com>
2016-04-06 16:32:13 -0400

    sweet, sweet bash scripts

--- facility_breakdown.sh
+++ facility_breakdown.sh
@@ -1,6 +1,9 @@
-FACILITIES=("CHICAGO MCC" "COOK COUNTY JAIL" "COOK COUNTY HOSPITAL" "CHICAGO HOLD ROOM" "US MARSHAL, N. DIST IL" "CATHOLIC CHARITIES, CHI" "TRAVELERS & IMMIGRANT AID" "HEARTLAND INTERNATIONAL YOUTH CTR")
+#!/bin/bash
+IFS=$'"'
+arr=($(./unique_facilities.sh $1 $2))
+unset IFS
 
-for facility in "${FACILITIES[@]}"
+for facility in "${arr[@]}"
 do
     echo ${facility}
     psql sanctuary -c "select det_facility, lift_reason, count(*) from requests where det_facility like '${facility}' and lift_reason is not null group by lift_reason, det_facility order by count desc;"
--- unique_facilities.sh
+++ unique_facilities.sh
@@ -1,1 +1,2 @@
-psql sanctuary -c "select distinct(det_facility) from requests where city = '$1' and state = '$2';"
+#!/bin/bash
+echo `psql sanctuary -c "COPY(select distinct(det_facility) from requests where city = '$1' and state = '$2') TO STDOUT;" | sed -e 's/^/"/;s/$/"/'` | tr '\n' ''
