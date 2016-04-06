d81fe386f0380af19e3c4fab3d9423d5b37e1a93
Tyler Fisher <tyler@tylerjfisher.com>
2016-04-06 16:36:56 -0400

    cleanup

--- facility_breakdown.sh
+++ facility_breakdown.sh
@@ -3,10 +3,8 @@ IFS=$'"'
 arr=($(./unique_facilities.sh $1 $2))
 unset IFS
 
-
 for facility in "${arr[@]}"
 do
     echo ${facility}
     psql sanctuary -c "select det_facility, lift_reason, count(*) from requests where det_facility like '${facility}' and lift_reason is not null group by lift_reason, det_facility order by count desc;"
-
 done
