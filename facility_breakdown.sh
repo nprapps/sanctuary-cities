a3a6629c329a5eee2b9c086d1d522a7a9b81dddf
Tyler Fisher <tyler@tylerjfisher.com>
2016-04-06 14:14:21 -0400

    loop over a list of facilities

--- facility_breakdown.sh
+++ facility_breakdown.sh
@@ -1,1 +1,7 @@
-psql sanctuary -c "select det_facility, lift_reason, count(*) from requests where det_facility = '$1' and lift_reason is not null group by lift_reason, det_facility order by count desc;"
+FACILITIES=("PACIFIC FURLOUGH FACILITY" "GEORGE BAILEY DETENT FACI" "GEO WESTERN REG. DET. FAC." "CORR.CORP.OF AMERICA - SAN DIEGO" "SAN DIEGO MCC" "S.D. COUNTY PROBATION" "CASA SAN JUAN" "SAN DIEGO COUNTY JAIL" "SAN DIEGO CENTRAL JAIL" "FACILITY 8 DETENTION FACILITY" "SAN DIEGO COUNTY JUV.HALL" "RJ DONOVAN CORREC FAC" "SND DISTRICT STAGING" "EAST MESA DETENT FACILITY")
+
+for facility in "${FACILITIES[@]}"
+do
+    echo ${facility}
+    psql sanctuary -c "select det_facility, lift_reason, count(*) from requests where det_facility = '${facility}' and lift_reason is not null group by lift_reason, det_facility order by count desc;"
+done
