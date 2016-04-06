bedbebe32a5b9ac3e73b18fac45183b80cb639c9
Zyma Islam <zyma.islam90@gmail.com>
2016-04-06 15:16:52 -0400

    adding facility breakdown

--- facility_breakdown.sh
+++ facility_breakdown.sh
@@ -1,7 +1,6 @@
-FACILITIES=("PACIFIC FURLOUGH FACILITY" "GEORGE BAILEY DETENT FACI" "GEO WESTERN REG. DET. FAC." "CORR.CORP.OF AMERICA - SAN DIEGO" "SAN DIEGO MCC" "S.D. COUNTY PROBATION" "CASA SAN JUAN" "SAN DIEGO COUNTY JAIL" "SAN DIEGO CENTRAL JAIL" "FACILITY 8 DETENTION FACILITY" "SAN DIEGO COUNTY JUV.HALL" "RJ DONOVAN CORREC FAC" "SND DISTRICT STAGING" "EAST MESA DETENT FACILITY")
-
+FACILITIES=("RIKERS ISLAND QUEENS NY")
 for facility in "${FACILITIES[@]}"
 do
     echo ${facility}
-    psql sanctuary -c "select det_facility, lift_reason, count(*) from requests where det_facility = '${facility}' and lift_reason is not null group by lift_reason, det_facility order by count desc;"
+    psql sanctuary -c "select det_facility, lift_reason, count(*) from requests where det_facility LIKE '${facility}' and lift_reason is not null group by lift_reason, det_facility order by count desc;"
 done
