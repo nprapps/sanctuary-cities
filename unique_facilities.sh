a9b76e3b00ca3737248ffc27c16ebef78d74e919
Tyler Fisher <tyler@tylerjfisher.com>
2016-04-06 16:32:52 -0400

    fix unique facilities query

--- unique_facilities.sh
+++ unique_facilities.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo `psql sanctuary -c "COPY(select distinct(det_facility) from requests where city = '$1' and state = '$2') TO STDOUT;" | sed -e 's/^/"/;s/$/"/'` | tr '\n' ''
+echo `psql sanctuary -c "COPY(select distinct(det_facility) from requests where city = '$1' and state = '$2') TO STDOUT;" | sed -e 's/^/"/;s/$/"/'`
