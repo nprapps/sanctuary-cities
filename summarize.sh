8912c69ee09bc19358c63bd6d352cd355b522a13
Zyma Islam <zyma.islam90@gmail.com>
2016-04-07 19:10:17 -0400

    added a table of numbers declined per year in each facility to numbers accepted not a pretty table

--- summarize.sh
+++ summarize.sh
@@ -16,9 +16,23 @@ inner join total_requests_per_facility as b
 on (a.det_facility = b.det_facility and a.city = b.city and a.state = b.state and b.count > 100);"
 
 psql sanctuary -c "create or replace view byyear as
-select to_char(date, 'YYYY') as year, det_facility,
-lift_reason, count(*) from requests where lift_reason = 'Detainer Declined by LEA'
-group by det_facility, year, lift_reason order by year, count desc;"
+select to_char(date, 'YYYY') as year, det_facility, city, state, count(*) from requests
+where lift_reason = 'Detainer Declined by LEA'
+and det_facility is not null
+group by year, det_facility, city, state
+order by year, count desc;"
 
+echo "Overall declined versus overall entertained"
 
-psql sanctuary -c "select * from byyear where year = '2014' order by count desc;"
+psql sanctuary -c "create or replace view everything_but_declined as
+select det_facility, to_char(date, 'YYYY') as year, count(lift_reason) from requests
+where lift_reason != 'Detainer Declined by LEA' and det_facility is not null
+group by det_facility, year order by det_facility desc;"
+
+psql sanctuary -c "COPY (select * from byyear a
+left outer join everything_but_declined b
+on a.det_facility = b.det_facility and a.year = b.year
+group by a.year,a.det_facility, a.city, a.state, a.count,
+b.det_facility, b.year, b.count
+order by b.year desc)
+TO '`pwd`/export/declined_vs_everythingelse.csv' DELIMITER ',' CSV HEADER;"
