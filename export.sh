b922de07624c38aa395afcc3d2cacc22d0995115
David Eads <davideads@gmail.com>
2016-12-19 19:42:37 -0500

    basic working system

--- export.sh
+++ export.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo "Stub"
+echo "Number of American citizens released from LEA detention"
+echo "select count(*) from requests where lift_reason='United States Citizen Interviewed'" | psql -t sanctuary
+
+echo "Number of American citizens released from ICE detention"
+python ice_detention_counter.py
